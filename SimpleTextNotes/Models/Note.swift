de5e5280882b1a0efa98bc3e659c2b61ab356fc5
Jannis Rondorf <github@jrondorf.de>
2026-02-20 20:36:29 +0100

    mac catalyst

--- SimpleTextNotes/Models/Note.swift
+++ SimpleTextNotes/Models/Note.swift
@@ -3,12 +3,17 @@ import SwiftData
 
 @Model
 class Note {
-    var id: UUID
-    var title: String
-    var content: String
-    var createdAt: Date
+    var id: UUID = UUID()
+    var title: String = ""
+    var content: String = ""
+    var createdAt: Date = Date()
 
-    init(id: UUID = UUID(), title: String = "", content: String = "", createdAt: Date = Date()) {
+    init(
+        id: UUID = UUID(),
+        title: String = "",
+        content: String = "",
+        createdAt: Date = Date()
+    ) {
         self.id = id
         self.title = title
         self.content = content
